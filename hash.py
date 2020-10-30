be75a2ece0b8d3e2faf2e70cc813c9396c59a472
Ines <ines.apablaza@alumnos.ucentral.cl>
2020-10-30 16:24:11 -0300

    Se han implementado funciones para averiguar al número primo más cercano

--- hash.py
+++ hash.py
@@ -1,41 +1,54 @@
-
-
 def double_hash(h1,h2, arreglo):
     aa = h1
     b=1
     while arreglo[aa] != -1:
         aa = (h1 + b* h2)%10
-        print(aa)
-        b=b+1
-        
+        b=b+1       
     return aa
 
+def esPrimo(x):
+    contador = 0
+    for f in range(1,x+1,1):
+        div = x % f
+        if div == 0:
+            contador = contador+1
+
+    if contador == 2:
+        return 1
+    else:
+        return 0
+
+def NumeroPrimoMasCercano(x):
+    for s in range(x,0,-1):
+        primo = esPrimo(s)
+        if primo == 1:
+            return s
+
 def hashing(arreglo_ingresado, arreglo_nuevo):
-    
-    for x in range (0,5,1):
+    largoAI = len(arreglo_ingresado)
+    for x in range (0,largoAI,1):
         elemento = arreglo_ingresado[x]
-        indice_elemento = int(elemento)%10
+        largoAN = len(arreglo_nuevo)
+        indice_elemento = int(elemento)%largoAN
         print("El indice es: ", indice_elemento," del elemento: ", elemento)
-        a=1
-        while arreglo_nuevo[indice_elemento]!= -1 and a==1:
+        
+        while arreglo_nuevo[indice_elemento]!= -1:
             print("OCURRIO UNA COLISIÓN EN EL INDICE")
-            calcu = int(elemento%7)
-            h2 = 7-calcu
-            
-            
+            Nprimo = NumeroPrimoMasCercano(largoAN)
+            calcu = int(elemento%Nprimo)
+            h2 = Nprimo-calcu
             indice_elemento = double_hash(indice_elemento,h2,arreglo_nuevo)
-            print("El indice real es:",indice_elemento, "del elemento:",elemento)
-            
-            a=9
+            print("El indice real es:",indice_elemento, "del elemento:",elemento)      
+           
         arreglo_nuevo[indice_elemento] = elemento
 
     return arreglo_nuevo
 
-arreglo = [5,25,15,35,95]    
+arregloIngresado = [5,25,15,35,95]    
 arreglo1 = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
 
 
-arreglo3 = hashing(arreglo, arreglo1)
+arreglo3 = hashing(arregloIngresado, arreglo1)
 
 
 

4999ec35ec690674f5afaf7b290f36491c814c92
Santiago <santiagosaav.99@gmail.com>
2020-10-31 21:52:36 -0300

    Se han agregado las tablas al interfaz, además de incorporar el hashing

--- hash.py
+++ hash.py
@@ -50,53 +50,20 @@ def get_ascii(palabra):
 # Función que realiza el Hashing
 # ____________________________________________________________
 def hashing(clave, valor, tabla_hash):
+    nuevo_indice=''
     largo_hash = len(tabla_hash)
     indice_elemento=get_ascii(clave)%largo_hash
-    print("El indice de: ", clave, "es: ", indice_elemento)
+    indice = ("El indice de " +str(clave)+ " es: "+ str(indice_elemento))
+    colision = ("No ha ocurrido ninguna colisión")
     while tabla_hash[indice_elemento]!=-1:
-        print("OCURRIÓ UNA COLISIÓN EN EL ÍNDICE")
+        colision = ("Ocurrió una colisión en el índice")
         numero_primo = NumeroPrimoMasCercano(largo_hash)
         calculo = int(get_ascii(clave)%numero_primo)
         ecuacion = numero_primo-calculo
         indice_elemento = double_hash(indice_elemento, ecuacion, tabla_hash)
-        print("El indice nuevo de: ", clave, "es: ", indice_elemento)
+        nuevo_indice=("El indice nuevo de "+str(clave)+ " es: "+str(indice_elemento))
     tabla_hash[indice_elemento] = valor
-    return tabla_hash
-# def hashing(arreglo_ingresado, arreglo_nuevo):
-#     largo_arreglo_ingresado = len(arreglo_ingresado)
-#     for x in range (0,largo_arreglo_ingresado,1):
-#         elemento = arreglo_ingresado[x]
-#         largo_arreglo_nuevo = len(arreglo_nuevo)
-#         indice_elemento = int(elemento)%largo_arreglo_nuevo
-#         print("El indice es: ", indice_elemento," del elemento: ", elemento)
-        
-#         while arreglo_nuevo[indice_elemento]!= -1:
-#             print("OCURRIO UNA COLISIÓN EN EL INDICE")
-#             numero_primo = NumeroPrimoMasCercano(largo_arreglo_nuevo)
-#             calculo = int(elemento%numero_primo)
-#             ecuacion_2 = numero_primo-calculo
-#             indice_elemento = double_hash(indice_elemento,ecuacion_2,arreglo_nuevo)
-#             print("El indice real es:",indice_elemento, "del elemento:",elemento)      
-           
-#         arreglo_nuevo[indice_elemento] = elemento
-#     return arreglo_nuevo
-
-tabla_hash = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
-clave = 'verde'
-valor = '#verde'
-clave1 = 'azul'
-valor1 = '#azul'
-clave2 = 'rojo'
-valor2 = '#rojo'
-clave3 = 'morado'
-valor3 = '#morado'
-clave4 = 'purpura'
-valor4 = '#purpura'
-hashing(clave, valor, tabla_hash)
-hashing(clave2, valor2, tabla_hash)
-hashing(clave3, valor3, tabla_hash)
-hashing(clave4, valor4, tabla_hash)
-print(tabla_hash)
+    return [tabla_hash,indice,colision, nuevo_indice]
 
 
 
--- interfaz.py
+++ interfaz.py
@@ -1,11 +1,34 @@
 from tkinter import *
+from hash import *
+#-------------------------------------------------------------------
+#          Creación de funciones
+#------------------------------------------------------------------
+def update_table(tabla_hash):
+    altura=110
+    for tabla_indice in range(0,10):
+        valor=Label(bg='white', width="20", height="1", text=tabla_hash[tabla_indice], font=("Cambria",12), fg="#22061A")
+        valor.place(x=510, y=altura)
+        altura+=30
+    return None
 
+def insertar_dato(clave, valor, tabla_hash):
+    arreglo_retorno = hashing(clave, valor, tabla_hash)
+    tabla_hash=arreglo_retorno[0]
+    if arreglo_retorno[2] == ("Ocurrió una colisión en el índice"):
+        consola=Label(bg='lightgray', width="30", height="5", text=arreglo_retorno[2]+'\n'+arreglo_retorno[3], font=("Cambria",12), fg="#22061A")
+        consola.place(x=750, y=200)
+    else:
+        consola=Label(bg='lightgray', width="30", height="5", text=arreglo_retorno[1], font=("Cambria",12), fg="#22061A")
+        consola.place(x=750, y=200)
+    update_table(tabla_hash)
+    return None
 #-------------------------------------------------------------------
 #          Creación de ventana
 #------------------------------------------------------------------
 ventana = Tk()
 ventana.geometry("1110x450")
 ventana['bg'] = '#6B0621'
+tabla_hash = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
 
 Titulo=Label(text="Tablas hash para colores HEX" ,font=("Cambria",16),fg="#CE7A6C" , bg="#330530", width="893", height="3")
 
@@ -28,7 +51,7 @@ titulo_codigo_hex.place(x=130, y=170)
 Entrada_codigo_hex = Entry(width="40", bg="#F3CCCD")
 Entrada_codigo_hex.place(x=64, y=200)
 
-btn_almacenar = Button(ventana, text="Almacenar", bg="#F3CCCD", command="#")
+btn_almacenar = Button(ventana, text="Almacenar", bg="#F3CCCD", command=lambda: insertar_dato(Entrada_nombre_color.get(),Entrada_codigo_hex.get(), tabla_hash))
 btn_almacenar.place(x=145, y=250)
 
 #-------------------------------------------------------------------
@@ -36,5 +59,14 @@ btn_almacenar.place(x=145, y=250)
 #------------------------------------------------------------------
 box_2=Label(bg="#AD3D66", width="100", height="21")
 box_2.place(x=375, y=100)
+
+altura = 110
+for creador_tabla_indice in range(0,10):
+    index=Label(bg='white', width="3", height="1", text=creador_tabla_indice, font=("Cambria",12), fg="#22061A")
+    index.place(x=430, y=altura)
+    altura+=30
+update_table(tabla_hash)
+consola=Label(bg='lightgray', width="30", height="5")
+consola.place(x=750, y=200)
 
 ventana.mainloop()
